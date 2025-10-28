9ddb2d68411f76c1cc650946e07bcf039c83b4fb
olahilario <grandejacare@gmail.com>
2025-10-27 23:02:09 -0300

    changed speed of last screen

--- src/trilha_01_AAU/left/11/scripts.js
+++ src/trilha_01_AAU/left/11/scripts.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const feed = document.querySelector(".image-feed-horizontal");
   const btnRestart = document.querySelector("#btn-restart");
 
-  const scrollSpeed = 0.8;
+  const scrollSpeed = 1.8;
   let touchStartX = 0;
 
   function handleKeyDown(event) {
