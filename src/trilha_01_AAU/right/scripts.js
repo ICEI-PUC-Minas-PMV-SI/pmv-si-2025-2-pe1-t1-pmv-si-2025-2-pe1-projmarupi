54496eefd795db356e2ac0d6efd1063057e76405
olahilario <grandejacare@gmail.com>
2025-10-30 21:03:16 -0300

    css and last screen direction scroll

--- src/trilha_01_AAU/right/scripts.js
+++ src/trilha_01_AAU/right/scripts.js
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", () => {
             ? event.deltaX
             : event.deltaY;
 
-        targetScroll += scrollAmount * wheelSensitivity;
+        targetScroll -= scrollAmount * wheelSensitivity;
 
         updateMaxScroll();
         if (targetScroll < 0) targetScroll = 0;
