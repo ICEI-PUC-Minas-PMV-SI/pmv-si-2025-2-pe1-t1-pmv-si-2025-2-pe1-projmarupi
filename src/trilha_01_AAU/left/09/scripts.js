7d064c668c1233214829a17ab6b39e1478b95a78
olahilario <grandejacare@gmail.com>
2025-11-13 00:43:21 -0300

    implemented the captcha and evaluation logic and some css and small issues

--- src/trilha_01_AAU/left/09/scripts.js
+++ src/trilha_01_AAU/left/09/scripts.js
@@ -1,7 +1,87 @@
-const btnVerifyList = document.querySelectorAll(".btn-verify");
+document.addEventListener("DOMContentLoaded", () => {
+  const allGridItems = document.querySelectorAll(".grid-item");
+  const btnsVerifyList = document.querySelectorAll(".btn-verify");
+  const btnsInfos = document.querySelectorAll(".btn-info");
 
-btnVerifyList.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    window.location.href = "../10/avaliacao.html";
+  allGridItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      item.classList.toggle("selected");
+    });
+  });
+
+  function checkAllCardsForWin() {
+    const successfulHeaders = document.querySelectorAll(".card-header.success");
+    const failHeaders = document.querySelectorAll(".card-header.error");
+
+    if (successfulHeaders.length === 3) {
+      setTimeout(() => {
+        window.location.href = "../10/avaliacao.html";
+      }, 500);
+    } else if (failHeaders.length === 3) {
+      alert("Precisamos aprender um pouco mais!");
+      window.location.href = "../05/quearvore.html";
+    }
+  }
+
+  btnsVerifyList.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const card = btn.closest(".card");
+      const cardHeader = card.querySelector(".card-header");
+      const correct = card.getAttribute("correctChar");
+      const gridItems = card.querySelectorAll(".grid-item");
+      let allCorrect = true;
+      let hasSelectedOneAtLeast = false;
+
+      cardHeader.classList.remove("error", "success");
+      btn.classList.remove("success");
+
+      gridItems.forEach((item) => {
+        const img = item.querySelector("img");
+        const src = img.getAttribute("src");
+        const filename = src.split("/").pop();
+
+        const isCorrectImage = filename.startsWith(correct);
+        const isSelected = item.classList.contains("selected");
+
+        if (isSelected) {
+          hasSelectedOneAtLeast = true;
+        }
+
+        if (isCorrectImage && !isSelected) {
+          allCorrect = false;
+        }
+
+        if (!isCorrectImage && isSelected) {
+          allCorrect = false;
+        }
+      });
+
+      if (!hasSelectedOneAtLeast) {
+        allCorrect = false;
+      }
+
+      if (allCorrect) {
+        cardHeader.classList.add("success");
+        btn.classList.add("success");
+        btn.textContent = "parabéns";
+      } else {
+        cardHeader.classList.add("error");
+        btn.textContent = "verificar";
+      }
+
+      checkAllCardsForWin();
+    });
+  });
+
+  btnsInfos[0].addEventListener("click", () => {
+    window.location.href = "../06/sibipiruna.html";
+  });
+
+  btnsInfos[1].addEventListener("click", () => {
+    window.location.href = "../06/acacia.html";
+  });
+
+  btnsInfos[2].addEventListener("click", () => {
+    window.location.href = "../06/ipe.html";
   });
 });
--- src/trilha_01_AAU/left/10/scripts.js
+++ src/trilha_01_AAU/left/10/scripts.js
@@ -1,7 +1,37 @@
-const listBtnCorrectAnswers = document.querySelectorAll(".correct-answer");
+document.addEventListener("DOMContentLoaded", () => {
+  const allAnswers = document.querySelectorAll(".nested-row");
 
-listBtnCorrectAnswers.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    window.location.href = "../11/inspiracao.html";
+  function checkWinCondition() {
+    const successfulHeaders = document.querySelectorAll(".card-row-1.success");
+    const wrongHeaders = document.querySelectorAll(".card-row-1.error");
+    if (successfulHeaders.length === 2) {
+      setTimeout(() => {
+        window.location.href = "../11/inspiracao.html";
+      }, 500);
+    } else if (wrongHeaders.length == 2) {
+      alert("Talvez seja melhor ler dessa vez!");
+      window.location.href = "../01/intro.html";
+    }
+  }
+
+  allAnswers.forEach((answer) => {
+    answer.addEventListener("click", () => {
+      const card = answer.closest(".card");
+      const cardHeader = card.querySelector(".card-row-1");
+
+      const answersInThisCard = card.querySelectorAll(".nested-row");
+      answersInThisCard.forEach((item) =>
+        item.classList.remove("selected", "wrong")
+      );
+      cardHeader.classList.remove("success", "error");
+      if (answer.classList.contains("correct-answer")) {
+        cardHeader.classList.add("success");
+        answer.classList.add("selected");
+      } else {
+        cardHeader.classList.add("error");
+        answer.classList.add("wrong");
+      }
+      checkWinCondition();
+    });
   });
 });
